1e7240f786d310d7774797bb17683b88266fe7b3
dikshab <dikshabansal2012@gmail.com>
2015-03-29 19:09:59 -0700

    Rain Sensor added
    Basic Code

---
+++ rainsensor/rainsensor.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+// initialize serial communication at 9600 bits per second:
+Serial.begin(9600);
+}
+
+
+void loop() {
+// read the input on analog pin 0:
+int sensorValue = analogRead(A0); Serial.println(sensorValue);
+delay(100);
+} 
+
+//when dry, the serial monitor prints 892. As it gets wet, the value decreases
